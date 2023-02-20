3ac075c4dee73075000c1cce4db81a1dfbb5f363
Alexey Sosnoviy <int-it@yandex.ru>
2023-02-20 13:24:00 +0300

    cover data small refact

--- internal/coverageData/coveragedata.go
+++ internal/coverageData/coveragedata.go
@@ -14,7 +14,7 @@ type Coverage struct {
 	Data map[string]map[int]bool
 }
 
-func Convert(parser *bslparser.Bslparser, reader *metareader.Metareader, fdbc *fdbc.Fdbc, basePath string) *Coverage {
+func Convert(LinesToCover map[string][]int, metaData map[metareader.Module]string, Storage map[fdbc.ModuleData][]fdbc.LIneCoverage, basePath string) *Coverage {
 
 	data := map[string]map[int]bool{}
 
@@ -23,7 +23,7 @@ func Convert(parser *bslparser.Bslparser, reader *metareader.Metareader, fdbc *f
 		log.Panic(err)
 	}
 
-	for k, v := range parser.LinesToCover {
+	for k, v := range LinesToCover {
 
 		linesToCover := map[int]bool{}
 		for _, notCoveredLines := range v {
@@ -35,12 +35,12 @@ func Convert(parser *bslparser.Bslparser, reader *metareader.Metareader, fdbc *f
 		data[sleshedpath] = linesToCover
 	}
 
-	for md, v := range fdbc.Storage {
+	for md, v := range Storage {
 		module := metareader.Module{ModuleUuid: md.ObjectID, ModuleType: md.PropertyID}
 		for _, lc := range v {
 			lineNo, _ := strconv.Atoi(lc.LineNo)
 
-			path, _ := filepath.Rel(cwd, reader.CoverData().Data[module])
+			path, _ := filepath.Rel(cwd, metaData[module])
 			sleshedpath := filepath.ToSlash(path)
 			data[sleshedpath][lineNo] = true
 		}
